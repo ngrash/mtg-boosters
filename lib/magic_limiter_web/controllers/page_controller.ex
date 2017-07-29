f778126462badf788c6778435cfdfabf62e26127
Nico Grashoff <ngrash@gmail.com>
2017-07-29 19:09:31 +0200

    Parse names and display result

--- lib/magic_limiter_web/controllers/page_controller.ex
+++ lib/magic_limiter_web/controllers/page_controller.ex
@@ -1,7 +1,20 @@
 defmodule MagicLimiterWeb.PageController do
   use MagicLimiterWeb, :controller
 
+  alias MagicLimiter.CardListParser
+
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  def create(conn, %{"pool_params" => pool_params}) do
+    case CardListParser.parse(pool_params["card_list"]) do
+      {:ok, pool} ->
+        render conn, "pool.html", cards: pool
+      {:error, :pool_empty} ->
+        conn
+        |> put_flash(:error, "Please enter a pool below")
+        |> redirect(to: page_path(conn, :index))
+    end
   end
 end
--- lib/magic_limiter_web/router.ex
+++ lib/magic_limiter_web/router.ex
@@ -17,6 +17,7 @@ defmodule MagicLimiterWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    post "/", PageController, :create
   end
 
   # Other scopes may use custom stacks.
