8f5425fda78974abded0bf0a986f92c7673be025
Nico Grashoff <ngrash@gmail.com>
2017-07-29 19:08:19 +0200

    Wrap result and be liberal with input

--- lib/magic_limiter/card_list_parser.ex
+++ lib/magic_limiter/card_list_parser.ex
@@ -1,30 +1,55 @@
 defmodule MagicLimiter.CardListParser do
   @doc """
+  iex> MagicLimiter.CardListParser.parse("Nissa, Genesis Mage")
+  {:ok, [{"Nissa, Genesis Mage", 1}]}
+
   iex> MagicLimiter.CardListParser.parse("1 Ahn-Crop Crasher")
-  [{"Ahn-Crop Crasher", 1}]
+  {:ok, [{"Ahn-Crop Crasher", 1}]}
 
   iex> MagicLimiter.CardListParser.parse("1 Kari Zev, Skyship Raider\\n2 Kari Zev's Expertise")
-  [{"Kari Zev's Expertise", 2}, {"Kari Zev, Skyship Raider", 1}]
+  {:ok, [{"Kari Zev's Expertise", 2}, {"Kari Zev, Skyship Raider", 1}]}
 
   iex> MagicLimiter.CardListParser.parse("1 Fireball\\n1 Counterlash\\n2 Fireball")
-  [{"Counterlash", 1}, {"Fireball", 3}]
+  {:ok, [{"Counterlash", 1}, {"Fireball", 3}]}
+
+  iex> MagicLimiter.CardListParser.parse("foo\\n\\nbar")
+  {:ok,[{"bar", 1}, {"foo", 1}]}
+
+  iex> MagicLimiter.CardListParser.parse("")
+  {:error, :pool_empty}
   """
-  def parse(pool) do
+  def parse(""), do: {:error, :pool_empty}
+  def parse(pool), do: {:ok, do_parse(pool)}
+
+  def do_parse(pool) do
     pool
     |> String.split("\n")
-    |> Enum.reduce(%{}, fn line, all ->
-      {name, count} = parse_line(line)
-      case Map.get(all, name) do
-        nil -> Map.put(all, name, count)
-        n   -> Map.put(all, name, count + n)
-      end
-    end)
+    |> Enum.reduce(%{}, &reducer/2)
     |> Map.to_list
+  end
+
+  defp reducer(line, collection) do
+    case String.trim(line) do
+      "" -> collection
+      _  ->
+        {name, count} =
+          line
+          |> String.trim
+          |> parse_line
+
+        case Map.get(collection, name) do
+          nil -> Map.put(collection, name, count)
+          n   -> Map.put(collection, name, count + n)
+        end
+    end
   end
 
   defp parse_line(line) do
     %{"count" => n, "name" => name} =
       Regex.named_captures(~r/((?<count>\d+) )?(?<name>.+)/, line)
-    {name, String.to_integer(n)}
+    case n do
+      "" -> {name, 1}
+      _  -> {name, String.to_integer(n)}
+    end
   end
 end
